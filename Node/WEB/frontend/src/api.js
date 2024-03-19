30911b9fb4cfa12922ee373b2df403a935faa8f8
Timmy Lu <melontimmy101@gmail.com>
2024-03-19 04:41:05 -0400

    attempted fix

--- Node/WEB/frontend/src/api.js
+++ Node/WEB/frontend/src/api.js
@@ -1,19 +1,33 @@
+
+
 const { SecretClient } = require("@azure/keyvault-secrets");
 const { DefaultAzureCredential } = require("@azure/identity");
 
+function getKeyVaultCredentials(){
+    return msRestAzure.loginWithAppServiceMSI({resource: 'https://cs519keyvault.vault.azure.net/'});
+}
+
+function getKeyVaultSecret(credentials) {
+    let keyVaultClient = new KeyVault.KeyVaultClient(credentials);
+    return keyVaultClient.getSecret(KEY_VAULT_URI, 'WebAppSecret', "");
+}
+
 async function API() {
-  const credential = new DefaultAzureCredential();
-  const keyVaultName = process.env["KEY_VAULT_NAME"];
-  const url = "https://" + keyVaultName + ".vault.azure.net";
-
-  const client = new SecretClient(url, credential);
-
-  const secret = await client.getSecret(secretName);
-
+    let secret1 = ""
+    getKeyVaultCredentials().then(getKeyVaultSecret).then(
+        function (secret){
+            secret1 = secret.value;
+        }
+    ).catch(
+        function (err) {
+            throw (err);
+        }
+    );
+    
   return (
    <div> 
     <p> test page 123 </p>
-    <p>The value of my secret is {secret}</p> </div>
+    <p>The value of my secret is {secret1}</p> </div>
   );
 }
 
