118e3d5c86f2f4e85c58265fd14f4dcd45d3ac67
Timmy Lu <melontimmy101@gmail.com>
2024-03-19 04:14:52 -0400

    fix2

--- Node/WEB/frontend/src/App.js
+++ Node/WEB/frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Routes, Router} from "react-router-dom";
+import {BrowserRouter, Routes, Router, Route} from "react-router-dom";
 import API from './api';
 
 function App() {
@@ -11,20 +11,6 @@ function App() {
                 <Route path="/api" element={<API/>} />
             </Routes>
         </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
