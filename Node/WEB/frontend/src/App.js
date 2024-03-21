e0d4f3a7be25fd1d3f0a7ea1d5dc4a18ef0e35b1
Timmy Lu <melontimmy101@gmail.com>
2024-03-21 00:48:42 -0400

    fixed routing i think

--- Node/WEB/frontend/src/App.js
+++ Node/WEB/frontend/src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
-import {Routes, Router, Route} from "react-router-dom";
+import {Switch, Route} from "react-router-dom";
 import API from './api.js';
 
 function App() {
   return (
     <div className="App">
-        <Router>
-            <Routes>
-                <Route path="/api" element={<API/>} />
-                <API/>
-            </Routes>
-        </Router>
+        <p>Main page, go to /api</p>
+        <Switch>
+            <Route path="/api" element={<API/>} />
+        </Switch>
+        <API/>
     </div>
   );
 }
