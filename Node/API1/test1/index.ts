5dc180420ec6fed142f33236d174f27e41fc7421
Timmy Lu <melontimmy101@gmail.com>
2024-03-21 03:12:04 -0400

    work on functions

--- Node/API1/test1/index.ts
+++ Node/API1/test1/index.ts
@@ -1,17 +1,29 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import {ManagedIdentityCredential} from "@azure/identity";
+import {SecretClient} from "@azure/keyvault-secrets"
+
+const credential = new ManagedIdentityCredential();
+
+const vaultName = "https://cs519keyvault.vault.azure.net/";
+
+
+const url = `https://${vaultName}.vault.azure.net`;
+const client = new SecretClient(url, credential);
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
     context.log('HTTP trigger function processed a request.');
     const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const webSecret = await client.getSecret("WebAppSecret");
+    const funcSecret = await client.getSecret("FunctionSecret");
+    console.log(funcSecret)
+    const responseMessage = webSecret
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
         body: responseMessage
     };
 
 };
 
 export default httpTrigger;
+
+
