e5b63d906e34d10b32a84659b23f533a0966f3d5
Timmy Lu <melontimmy101@gmail.com>
2024-03-21 01:25:51 -0400

    aaaaaa

--- Node/WEB/frontend/src/index.js
+++ Node/WEB/frontend/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import {API} from "./components/API"
+import API from "./components/API.js"
 
 ReactDOM.render(
   <Router>
